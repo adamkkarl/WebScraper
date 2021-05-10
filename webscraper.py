832185f81afb43c521c0f7d18c35008bc54137cb
adamkkarl <adamkkarl@gmail.com>
2021-05-09 21:37:10 -0400

    repeatedly checks until 5 transactions in 5 mins

--- webscraper.py
+++ webscraper.py
@@ -1,16 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
+import time
 from urllib.request import Request, urlopen
 
-req = Request('https://bitinfocharts.com/dogecoin/address/DG2mPCnCPXzbwiqKpE1husv3FA9s5t1WMt', headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req)
+
+while(True):
+    print("checking.....")
+
+    req = Request('https://bitinfocharts.com/dogecoin/address/DG2mPCnCPXzbwiqKpE1husv3FA9s5t1WMt', headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req)
 
 
-# print(req.status_code)
-content = BeautifulSoup(webpage, "html.parser")
+    # print(req.status_code)
+    content = BeautifulSoup(webpage, "html.parser")
 
-for c in content.find_all('td', attrs={"class": "text-error"}):
-    print(c.text)
+    # finds amounts transfered
+    # for c in content.find_all('td', attrs={"class": "text-error"}):
+    #     print(c.text)
 
+    times = content.find_all('td', attrs={"class": "utc hidden-phone"})
+    latest = times[0].text[:-4]
+    fifth = times[5].text[:-4]
 
-#//*[@id="table_maina"]/tbody/tr[1]/td[3]/text()
+    l = datetime.datetime.strptime(latest, '%Y-%m-%d %H:%M:%S')
+    f = datetime.datetime.strptime(fifth, '%Y-%m-%d %H:%M:%S')
+    difference = (l-f).total_seconds()
+
+    if difference/60 < 5:
+        # 5+ transactions in 5 mins
+        print("CHECK WALLET POP")
+        exit()
+
+    time.sleep(90)
