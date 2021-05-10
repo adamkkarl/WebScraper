076e8e6914e23d396a2b698e4ead2d766333f16d
adamkkarl <adamkkarl@gmail.com>
2021-05-09 21:07:07 -0400

    scrapes site, prints transactions

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.request import Request, urlopen
+
+req = Request('https://bitinfocharts.com/dogecoin/address/DG2mPCnCPXzbwiqKpE1husv3FA9s5t1WMt', headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req)
+
+
+# print(req.status_code)
+content = BeautifulSoup(webpage, "html.parser")
+
+for c in content.find_all('td', attrs={"class": "text-error"}):
+    print(c.text)
+
+
+#//*[@id="table_maina"]/tbody/tr[1]/td[3]/text()
