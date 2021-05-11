70e956776ce4bb47cddbf58e61e2f59269888b6d
adamkkarl <adamkkarl@gmail.com>
2021-05-11 00:36:08 -0400

    added text message functionality

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WebScraper
 messing around learning how to build a web scraper for general use
+
+
+Install Virtual Environment (Windows command line):
+$ python3 -m venv env
+$ env\Scripts\activate.bat
+
+
+Inside Virtual Environment
+$ pip install bs4  <!--- beautiful soup --->
+$ pip install requests <!--- requests --->
+
+Setup keyring
+$ pip install keyring
+in python
+keyring.set_password("twilio", "ACdaa60d580e7f089ec6d04fc5583071ad", "INSERT AUTH TOKEN")
+keyring.set_password("phone", "ACdaa60d580e7f089ec6d04fc5583071ad", "INSERT PHONE NUMBER")
--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
-import requests
-import datetime
-import time
+import requests,  datetime, time, keyring
 from urllib.request import Request, urlopen
+from twilio.rest import Client
 
 
 while(True):
@@ -10,7 +9,6 @@ while(True):
 
     req = Request('https://bitinfocharts.com/dogecoin/address/DG2mPCnCPXzbwiqKpE1husv3FA9s5t1WMt', headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req)
-
 
     # print(req.status_code)
     content = BeautifulSoup(webpage, "html.parser")
@@ -29,6 +27,20 @@ while(True):
 
     if difference/60 < 5:
         # 5+ transactions in 5 mins
+
+        #twilio id, not confidential
+        account_sid = "ACdaa60d580e7f089ec6d04fc5583071ad"
+
+        #use keyring for private authentication and phone number
+        auth_token = keyring.get_password("twilio", account_sid)
+        my_number = keyring.get_password("phone", account_sid)
+
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            to=my_number,
+            from_="+19563202956",
+            body="CHECK WALLET POP")
+
         print("CHECK WALLET POP")
         exit()
 
